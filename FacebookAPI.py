7f86446f34b48fe01db879bb903864c1c3ab538c
mohamed abdallah <mo7amed.3bdalla7@gmail.com>
2017-08-12 00:17:32 +0200

    intial commit

---
+++ FacebookAPI.py
@@ -0,0 +1,242 @@
+# encoding=utf8
+import requests, json
+from flask import url_for
+
+
+def get_user_fb(token, user_id):
+    r = requests.get("https://graph.facebook.com/v2.6/" + user_id,
+                     params={"fields": "first_name,last_name,profile_pic,locale,timezone,gender"
+                         , "access_token": token
+                             })
+    if r.status_code != requests.codes.ok:
+        print r.text
+        return
+    user = json.loads(r.content)
+    return user
+
+
+def show_typing(token, user_id, action='typing_on'):
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                      params={"access_token": token},
+                      data=json.dumps({
+                          "recipient": {"id": user_id},
+                          "sender_action": action
+                      }),
+                      headers={'Content-type': 'application/json'})
+    if r.status_code != requests.codes.ok:
+        print r.text
+
+
+def send_message(token, user_id, text):
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                      params={"access_token": token},
+                      data=json.dumps({
+                          "recipient": {"id": user_id},
+                          "message": {"text": text.decode('unicode_escape')}
+                      }),
+                      headers={'Content-type': 'application/json'})
+    if r.status_code != requests.codes.ok:
+        print r.text
+
+
+def send_picture(token, user_id, imageUrl, title="", subtitle=""):
+    if title != "":
+        data = {"recipient": {"id": user_id},
+                "message": {
+                    "attachment": {
+                        "type": "template",
+                        "payload": {
+                            "template_type": "generic",
+                            "elements": [{
+                                "title": title,
+                                "subtitle": subtitle,
+                                "image_url": imageUrl
+                            }]
+                        }
+                    }
+                }
+                }
+    else:
+        data = {"recipient": {"id": user_id},
+                "message": {
+                    "attachment": {
+                        "type": "image",
+                        "payload": {
+                            "url": imageUrl
+                        }
+                    }
+                }
+                }
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                      params={"access_token": token},
+                      data=json.dumps(data),
+                      headers={'Content-type': 'application/json'})
+    if r.status_code != requests.codes.ok:
+        print r.text
+
+
+def send_url(token, user_id, text, title, url):
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                      params={"access_token": token},
+                      data=json.dumps({
+                          "recipient": {"id": user_id},
+                          "message": {
+                              "attachment": {
+                                  "type": "template",
+                                  "payload": {
+                                      "template_type": "button",
+                                      "text": text,
+                                      "buttons": [
+                                          {
+                                              "type": "web_url",
+                                              "url": url,
+                                              "title": title
+                                          }
+                                      ]
+                                  }
+                              }
+                          }
+                      }),
+                      headers={'Content-type': 'application/json'})
+    if r.status_code != requests.codes.ok:
+        print r.text
+
+
+def send_intro_screenshots(app, token, user_id):
+    chat_speak = {
+        "title": 'You can both chat and speak to me',
+        "image_url": url_for('static', filename="assets/img/intro/1-voice-and-text.jpg", _external=True),
+        "subtitle": 'I understand voice and natural language (I try to be smarter everyday :D)'
+    }
+    location_text = {
+        "title": "Find a restaurant/shop for you",
+        "image_url": url_for('static', filename="assets/img/intro/2-yelp-gps-location.jpg", _external=True),
+        "subtitle": "Tell me what you want, then your location name, address or GPS"
+    }
+    location_gps = {
+        "title": "In case you've never sent location in Messenger",
+        "image_url": url_for('static', filename="assets/img/intro/3-how-to-send-location.jpg", _external=True),
+        "subtitle": "GPS will be the best option, but just a distinctive name would do",
+    }
+    location_save = {
+        "title": "Save your favorite locations",
+        "image_url": url_for('static', filename="assets/img/intro/4-save-location.jpg", _external=True),
+        "subtitle": "Make it convenient for you"
+    }
+    memo1 = {
+        "title": "Say \"Memorize\" or \"Memorize this for me\"",
+        "image_url": url_for('static', filename="assets/img/intro/5-memo.jpg", _external=True),
+        "subtitle": "Then your memo in the same/separate message"
+    }
+    news = {
+        "title": "Keep you updated",
+        "image_url": url_for('static', filename="assets/img/intro/6-news.jpg", _external=True),
+        "subtitle": "With the most trending news"
+    }
+
+    options = [chat_speak, location_text, location_gps, location_save, memo1, news]
+
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                      params={"access_token": token},
+                      data=json.dumps({
+                          "recipient": {"id": user_id},
+                          "message": {
+                              "attachment": {
+                                  "type": "template",
+                                  "payload": {
+                                      "template_type": "generic",
+                                      "elements": options
+                                  }
+                              }
+                          }
+                      }),
+                      headers={'Content-type': 'application/json'})
+    if r.status_code != requests.codes.ok:
+        print r.text
+
+
+def send_quick_replies(token, user_id, intro):
+    quickRepliesOptions = [
+        {"content_type": "text",
+         "title": "Aguamenti",
+         "payload": 'spell-aguamenti'
+         },
+        {"content_type": "text",
+         "title": "Expecto Patronum",
+         "payload": 'spell-expecto-patronum'
+         },
+        {"content_type": "text",
+         "title": "Avada Kedavra",
+         "payload": 'spell-avada-kedavra'
+         },
+        {"content_type": "text",
+         "title": "Alohomora",
+         "payload": 'spell-alohomora'
+         }
+    ]
+    data = json.dumps({
+        "recipient": {"id": user_id},
+        "message": {
+            "text": intro,
+            "quick_replies": quickRepliesOptions
+        }
+    })
+    data = data.encode('utf-8')
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                      params={"access_token": token},
+                      data=data,
+                      headers={'Content-type': 'application/json'})
+
+    if r.status_code != requests.codes.ok:
+        print r.text
+
+
+def set_menu(token):
+    r = requests.post("https://graph.facebook.com/v2.6/me/messenger_profile",
+                      params={"access_token": token},
+                      data=json.dumps({
+                          "setting_type": "call_to_actions",
+                          "thread_state": "existing_thread",
+                          "call_to_actions": [
+                              {
+                                  "type": "postback",
+                                  "title": "الئمة",
+                                  "payload": "Akla_Menu"
+                              },
+                              {
+                                  "type": "postback",
+                                  "title": "الطلبات",
+                                  "payload": "Akla_Orders"
+                              },
+                              {
+                                  "type": "postback",
+                                  "title": "حسابي",
+                                  "payload": "Akla_Account"
+                              }
+                          ]
+                      }),
+                      headers={'Content-type': 'application/json'})
+    print r.content
+    if r.status_code != requests.codes.ok:
+        print r.text
+
+
+def set_get_started_button(token):
+    r = requests.post("https://graph.facebook.com/v2.6/me/thread_settings",
+                      params={"access_token": token},
+                      data=json.dumps({
+                          "setting_type": "call_to_actions",
+                          "thread_state": "new_thread",
+                          "call_to_actions": [
+                              {
+                                  "payload": "Get_Started_Button"
+                              }
+                          ]
+                      }),
+                      headers={'Content-type': 'application/json'})
+    print r.content
+    if r.status_code != requests.codes.ok:
+        print r.text
+
+        # set_menu()
+        # set_get_started_button()
---
+++ app.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import json
+
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+    # endpoint for processing incoming messaging events
+
+    data = request.get_json()
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+                    pass
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
+
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print str(message)
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
