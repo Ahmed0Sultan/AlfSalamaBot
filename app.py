e70029a49d903f1e5f3f43290acd0fa5ae31fb22
Ahmed0Sultan <ahmed.khaled4941@gmail.com>
2017-09-15 23:30:02 +0200

    Added Test

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ db.create_all()
 
 def parts_slicer(parts):
     parts_num = len(parts)
-    if parts_num > 0:
+    if parts_num > 10:
         num_of_iterates = int(math.floor(parts_num / 10))
         parts_dict = {}
         if parts_num > 0:
