c55cddad34ac0df14e1feb6f77aea8e0bc7af33a
Ahmed0Sultan <ahmed.khaled4941@gmail.com>
2017-09-15 02:40:48 +0200

    Added Test

--- app.py
+++ app.py
@@ -318,7 +318,6 @@ def quickReplyProcessing(user_id,quick_reply_payload):
         FB.show_typing(token, user_id, 'typing_on')
         intro = u"من فضلك ادخل بيانات هذا الشخص لتساعدنا على تقديم افضل خدمة"
         FB.send_complete_data_button(token, user_id, intro,False)
-        FB.send_where_to_go_quick_replies(token,user_id,u"من فضلك اختر الى اين تريد الذهاب")
     elif quick_reply_payload == "Choose_Who_To_Diagnose":
         FB.show_typing(token, user_id, 'typing_on')
         FB.send_whose_diagnoses(token, user_id, u"هل هذا التشخيص لك ام لشخص اخر ؟")
