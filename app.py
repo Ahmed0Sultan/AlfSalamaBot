ba49b67b6b2be0e87295f73a29875d9da07a7426
Ahmed0Sultan <ahmed.khaled4941@gmail.com>
2017-09-21 02:22:49 +0200

    Added Test

--- app.py
+++ app.py
@@ -495,7 +495,7 @@ def quickReplyProcessing(user_id,quick_reply_payload):
         # FB.send_message(token,user_id,u"من فضلك اختر العضو الذى تشكو منه")
         print 'List issssssss '+ str(parts_list)
         FB.show_typing(token, user_id, 'typing_on')
-        FB.send_body_parts(token, user_id,parts_list)
+        FB.send_body_parts_test(token, user_id,parts_list)
         if list_part_number < num_of_iterates:
             FB.show_typing(token, user_id, 'typing_on')
             FB.send_more_body_parts_quick_replies(token, user_id,u"من فضلك اختر العضو الذى تشكو منه",list_part_number+1)
